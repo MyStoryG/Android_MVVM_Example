1c4b2e13a11a110718e421f5ec46dbc2dcc3bea8
MyStoryG <bookwriterseo@gmail.com>
2022-07-16 02:31:54 +0900

    ViewModel 생명주기 적용

--- app/src/main/java/com/mvvm/example/view/OrderActivity.kt
+++ app/src/main/java/com/mvvm/example/view/OrderActivity.kt
@@ -3,16 +3,18 @@ package com.mvvm.example.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.mvvm.example.R
 import com.mvvm.example.databinding.ActivityOrderBinding
 import com.mvvm.example.viewmodel.OrderViewModel
 
 class OrderActivity : AppCompatActivity() {
-    private val viewModel: OrderViewModel = OrderViewModel()
+    private lateinit var viewModel: OrderViewModel
     private lateinit var binding: ActivityOrderBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_order)
         binding.lifecycleOwner = this
         binding.counter = viewModel
